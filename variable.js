5f07624f9525b3581c6b88d6db5f56c73ebf98a1
Monika Jadhav <monikajadhav1211@gmail.com>
2026-05-12 13:23:45 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,21 @@
+let name = "Monika";
+console.log(name);
+
+//number variables
+let a = 10;
+let b = 20;
+console.log(a+b);
+
+//student marks
+let marks = 80;
+console.log(marks);
+marks=95;
+console.log(marks);
+
+//string variables
+let username = "mona";
+console.log("welcome " + username);
+
+//boolean variables
+let isLoggedIn = true;
+console.log(isLoggedIn);
