d98ea5ff1396581443b2135eb95f840c0da1baf3
Monika Jadhav <monikajadhav1211@gmail.com>
2026-05-16 22:30:38 +0530

    Add files via upload

---
+++ string1.js
@@ -0,0 +1,32 @@
+// Using Single Quote
+let s1 = 'abcd';
+console.log(s1);
+
+// Using Double Quote
+let s2 = "abcd";
+console.log(s2);
+
+//create using constructor
+let s = new String('abcd');
+console.log(s);
+
+//string interpolation
+let s3 = 'gfg';
+let s4 = `You are learning from ${s3}`;
+
+console.log(s4);
+
+//empty string
+let s5 = '';
+let s6 = "";
+
+console.log(s5);
+console.log(s6);
+
+//multiline string
+let s7 = `
+    This is a
+    multiline
+    string`;
+
+console.log(s7);
