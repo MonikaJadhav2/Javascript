7897f692a3fb2e29826f658a518a1304d6a60762
Monika Jadhav <monikajadhav1211@gmail.com>
2026-05-20 19:41:22 +0530

    Add files via upload

---
+++ Objects1.js
@@ -0,0 +1,22 @@
+const student={
+    name:"Monika",
+    age:27,
+    city:"Pune"
+};
+console.log(student);
+
+let obj = { name: "Sourav", age: 22 };
+console.log(obj);
+
+obj.age = 23;
+console.log(obj);
+console.log("---------------------------------------");
+
+let obj1 = { model: "Tesla" };
+obj1.color = "Red";
+
+console.log(obj);
+console.log("---------------------------------------");
+console.log("---------------------------------------");
+console.log("---------------------------------------");
+console.log("---------------------------------------");
